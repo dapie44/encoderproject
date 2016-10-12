e1d7e8dd6f539f3ad3b8bbd24e4a0da22a47c55c
Anthony Sandoval <retrovirus546@gmail.com>
2016-10-12 14:03:23 -0700

    tried to put echo

--- encoder.js
+++ encoder.js
@@ -3,10 +3,38 @@ $("#divname").keydown(function(e) {
    e.keyCode == 13
 });
 
-$("input[type='radio']").click(function(){
+
+$("input[type='radio']").keydown(function(){
   $("input:checked").prop('checked', false);
   $(this).prop('checked', true);
 
 });
 
-if ($("input:changed").val() == 'echo')
+if ($("input:changed").val() == 'echo') {
+  65 : "a";
+  66 : "b";
+  67 : "c";
+  68 : "d";
+  69 : "e";
+  70 : "f";
+  71 : "g";
+  72 : "h";
+  73 : "i";
+  74 : "j";
+  75 : "k";
+  76 : "l";
+  77 : "m";
+  78 : "n";
+  79 : "o";
+  80 : "p";
+  81 : "q";
+  82 : "r";
+  83 : "s";
+  84 : "t";
+  85 : "u";
+  86 : "v";
+  87 : "w";
+  88 : "x";
+  89 : "y";
+  90 : "z";
+}
